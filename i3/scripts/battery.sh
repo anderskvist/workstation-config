0d88bebe398528d66daddbd78e2f2bf6000c6908
Anders Kvist <anderskvist@gmail.com>
2017-05-21 09:55:54 +0200

    Only show low battery warnings if the battery is discharging

--- i3/scripts/battery.sh
+++ i3/scripts/battery.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+STATUS=$(cat /sys/class/power_supply/BAT0/status)
 CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
 BATTINFO=$(acpi -b)
 
-if [ ${CAPACITY} -lt 5 ]; then
-    DISPLAY=:0.0 /usr/bin/notify-send -i battery-empty -u critical "Battery critical!" "${BATTINFO}"
-elif [ ${CAPACITY} -lt 15 ]; then
-    DISPLAY=:0.0 /usr/bin/notify-send -i battery-low -u normal "Battery warning!" "${BATTINFO}"
+
+if [ "${STATUS}" = "Discharging" ]; then
+    if [ ${CAPACITY} -lt 5 ]; then
+        DISPLAY=:0.0 /usr/bin/notify-send -i battery-empty -u critical "Battery critical!" "${BATTINFO}"
+    elif [ ${CAPACITY} -lt 15 ]; then
+        DISPLAY=:0.0 /usr/bin/notify-send -i battery-low -u normal "Battery warning!" "${BATTINFO}"
+    fi
 fi
