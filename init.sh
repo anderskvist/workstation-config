5d4aec19e872002242472929ad728850be7e3eb3
Anders Kvist <ak@cego.dk>
2017-06-07 11:29:45 +0200

    Added spotify

--- init.sh
+++ init.sh
@@ -74,3 +74,9 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install docker-ce python-pip -f -y
 sudo pip install docker-compose
+
+# spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
