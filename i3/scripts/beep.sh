df867b6f88fb5c27c2e89803c3f416962ca8065a
Anders Kvist <ak@cego.dk>
2018-11-14 15:43:21 +0100

    Added quick and dirty system beep command that handles muted and low volume settings via alsa.

---
+++ i3/scripts/beep.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# This works for a ThinkPad X1 Carbon 5th generation
+# Requires pcspkr module is loaded and beep command installed
+
+MASTERMUTE=$(amixer cget numid=11|grep "  : values"|cut -d"=" -f2)
+MASTERVOLUME=$(amixer cget numid=10|grep "  : values"|cut -d"=" -f2)
+PCMVOLUME=$(amixer cget numid=55|grep "  : values"|cut -d"=" -f2)
+SPEAKERMUTE=$(amixer cget numid=4|grep "  : values"|cut -d"=" -f2)
+SPEAKERVOLUME=$(amixer cget numid=3|grep "  : values"|cut -d"=" -f2)
+
+amixer cset numid=11 on
+amixer cset numid=10 100
+amixer cset numid=55 0
+amixer cset numid=4 on
+amixer cset numid=3 100
+
+beep
+
+amixer cset numid=11 ${MASTERMUTE}
+amixer cset numid=10 ${MASTERVOLUME}
+amixer cset numid=55 ${PCMVOLUME}
+amixer cset numid=4 ${SPEAKERMUTE}
+amixer cset numid=3 ${SPEAKERVOLUME}
