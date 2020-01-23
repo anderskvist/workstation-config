e3459dafffe0c72149cca40587313f11d6342fa2
Anders Kvist <ak@cego.dk>
2020-01-23 19:06:34 +0100

    Setting to all current sinks when muting and changing volume.

--- scripts/mute.sh
+++ scripts/mute.sh
@@ -4,6 +4,8 @@ pactl set-sink-mute 0 toggle
 
 MUTE=$(pactl list sinks | sed -n '/^Sink #0/,/Mute:/p'|tail -n 1|awk '{print $2}')
 
-pactl set-sink-mute 15 ${MUTE}
+for SINK in $(pactl list sinks short|awk '{print $1}'); do
+  pactl set-sink-mute ${SINK} ${MUTE}
+done
 
 
--- scripts/volume.sh
+++ scripts/volume.sh
@@ -5,7 +5,7 @@ pactl set-sink-volume 0 ${1}
 # Get current channel volume as average
 AVG=$(pactl list sinks | sed -n '/^Sink #0/,/Volume:/p'|tail -n 1|awk -F"/" '{print ($2+$4)/2}')
 
-pactl set-sink-volume 0 ${AVG}%
-pactl set-sink-volume 15 ${AVG}%
-
+for SINK in $(pactl list sinks short|awk '{print $1}'); do
+  pactl set-sink-volume ${SINK} ${AVG}%
+done
 
