bbb808588181c185307ff6474fae80fbb3528f3c
Anders Kvist <a.kvist@elsevier.com>
2022-11-08 19:08:53 +0100

    blanking screen when locking it.

--- scripts/lock_screen.sh
+++ scripts/lock_screen.sh
@@ -6,5 +6,8 @@ scrot -o /tmp/screen_locked.png
 # Pixellate it 10x
 mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 
+# Blank
+blank.sh &
+
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
